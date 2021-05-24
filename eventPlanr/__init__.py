db51320298d6320c1ee46fdb09a3aa082707f5ec
“kjnikhil6” <kjnikhil006@gmail.com>
2021-05-24 09:05:19 +0530

    make it better

--- eventPlanr/__init__.py
+++ eventPlanr/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wgathkgilknakc:e1c5d33d3b99038d6cb49c64cba5e1b7386c6b13e94f41c8b615fb3185a4384a@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d5pn0s9rcig3i6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mnfymrkkknsuip:8aa0a12a9e31a42615e39e132e8077c0bbf21c912267ef00c443d08513f7c8a6@ec2-3-215-57-87.compute-1.amazonaws.com:5432/dd3tq83m1u5ukk'
 db = SQLAlchemy(app)
 bcrypt= Bcrypt(app)
 login_manager=LoginManager(app)
